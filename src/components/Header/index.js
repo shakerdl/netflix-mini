165587a4b92203577dc003197e8c24d11aaa0d06
Tal Cohen <shakerdl120@gmail.com>
2023-05-05 14:54:09 +0300

    register Done, added Login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,39 +4,49 @@ import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.leftHeader}>
-
-<div><Link to="/home">home Page</Link></div>
-
-                <div><Link to="/series">Series</Link></div>
-                <div><Link to="/movies">
-                Movies
-                </Link>
-                    </div>
-                <div>Trending</div>
-                <div>My list</div>
-                <div><Link to="/search">Search</Link></div>
-                <div><Link to="/favorites">Favorites</Link></div>
-            </div>
-            <div className={styles.rightRight}>
-                <nav>
-                    <ul>
-                        <li><a>Home page</a></li>
-                        <li><a>Tv shows</a></li>
-                        <li><a>Movies</a></li>
-                    </ul>
-                </nav>
-                <div className={styles.logoContainer}>
-                    <img src={logo} alt="Logo" />
-                </div>
-            </div>
-            <Routes>
-                {/* <Route path="/search" element={<Search />}></Route> */}
-                {/* <Route path="/favorites" element={<Favorites />}></Route> */}
-            </Routes>
-        </header>
-    )
-}
-
+  return (
+    <header className={styles.header}>
+      <div className={styles.leftHeader}>
+        <div>
+          <Link to="/">home Page</Link>
+        </div>
+        <div>
+          <Link to="/series">Series</Link>
+        </div>
+        <div>
+          <Link to="/movies">Movies</Link>
+        </div>
+        <div>Trending</div>
+        <div>My list</div>
+        <div>
+          <Link to="/search">Search</Link>
+        </div>
+        <div>
+          <Link to="/favorites">Favorites</Link>
+        </div>
+      </div>
+      <div className={styles.rightRight}>
+        <nav>
+          <ul>
+            <li>
+              <a>Home page</a>
+            </li>
+            <li>
+              <a>Tv shows</a>
+            </li>
+            <li>
+              <a>Movies</a>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="Logo" />
+        </div>
+      </div>
+      <Routes>
+        {/* <Route path="/search" element={<Search />}></Route> */}
+        {/* <Route path="/favorites" element={<Favorites />}></Route> */}
+      </Routes>
+    </header>
+  );
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Hero } from "../../containers/Hero";
-import Features from "../../containers/Features";
+// import Features from "../../containers/Features";
 import Footer from "../../components/Footer";
+import List from "../../components/List";
 
 const Home = () => {
   return (
     <>
       <Hero>Main Clip</Hero>
-      <Features>Features</Features>
+      {/* <Features>Features</Features> */}
+      <List/>
       <Footer>Footer</Footer>
     </>
   );
