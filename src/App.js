c9eb5771ef818ebfc7566648770d577670b045c7
Tal Cohen <shakerdl120@gmail.com>
2023-05-05 18:52:02 +0300

    under building

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import styles from "./App.module.css";
-import { Route, Routes,Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Search from "./components/Search";
 import Favorites from "./components/List";
@@ -10,33 +10,35 @@ import Login from "./pages/Login";
 
 function App() {
   const user = true;
-  
-  
-  const validUser = (currentUser,type)=>{
+
+  const validUser = (currentUser, type) => {
     if (currentUser) {
-      return <Home/>
+      return <Home />;
     }
     if (type === "register") {
-      return <Register/>
+      return <Register />;
     }
     if (type === "login") {
-      return <Login/>
+      return <Login />;
     }
-    return <Register/>
-  }
- 
+    return <Register />;
+  };
+
   return (
     <div className={styles.container}>
-      <Header>Header</Header>
       <Routes>
         <Route exact path="/" element={validUser(user)}></Route>
-        <Route path="/register" element={validUser(!user,"register")}></Route>
-        <Route path="/login" element={validUser(!user,"login")}></Route>
-        <Route path="/movies" element={<Home type="movies" />}></Route>
-        <Route path="/series" element={<Home type="series" />}></Route>
-        {/* <Route path="/watch" element={<Watch />}></Route>  //it's for the video to open in full screen */}
-        <Route path="/search" element={<Search />}></Route>
-        <Route path="/favorites" element={<Favorites />}></Route>
+        <Route path="/register" element={validUser(!user, "register")}></Route>
+        <Route path="/login" element={validUser(!user, "login")}></Route>
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />}></Route>
+            <Route path="/series" element={<Home type="series" />}></Route>
+            {/* <Route path="/watch" element={<Watch />}></Route>  //it's for the video to open in full screen */}
+            <Route path="/search" element={<Search />}></Route>
+            <Route path="/favorites" element={<Favorites />}></Route>
+          </>
+        )}
       </Routes>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import { Hero } from "../../containers/Hero";
 // import Features from "../../containers/Features";
 import Footer from "../../components/Footer";
 import List from "../../components/List";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({type}) => {
+  const [Lists,setLists] = useState([])
+  const [genre,setGenre] = useState(null)
+
+  useEffect(()=>{
+const getRandomLists = async ()=>{
+  try {
+     const res = await axios.get(`Lists${type && "?type="+ type}&${genre && "genre="+ genre}` )
+  } catch (err) {
+    console.log(err);
+  }
+}
+  },[type,genre])
   return (
     <>
+      <Header>Header</Header>
       <Hero>Main Clip</Hero>
       {/* <Features>Features</Features> */}
-      <List/>
+      <List />
       <Footer>Footer</Footer>
     </>
   );
