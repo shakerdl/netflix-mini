37576fe0d4eef66cc14ed092d81e33a74b583a04
Tal Cohen <shakerdl120@gmail.com>
2023-05-05 14:34:31 +0300

    listComponent Done

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 
-const ListItem = () => {
+const ListItem = ({index}) => {
     const [isHovered, setIsHovered] = useState(false);
     const trailer =
       "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
